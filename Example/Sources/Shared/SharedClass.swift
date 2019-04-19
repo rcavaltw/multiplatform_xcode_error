a1ead1d344193fef0ecbd414bf452c4fb7313751
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-04-19 19:20:32 -0300

    change project structure in order to have separate classes per platform

---
+++ Example/Sources/Shared/SharedClass.swift
@@ -0,0 +1,9 @@
+//
+//  SharedClass.swift
+//  Example
+//
+//  Created by Ricardo Cavalcanti on 19/04/19.
+//  Copyright © 2019 Example org. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Sources/tvOS/TvOsClass.swift
@@ -0,0 +1,8 @@
+class TVOsClass {
+    
+    var tvOsOnly: AVFoundationNowPlayingBuilder?
+    
+    init(_ tvos: AVFoundationNowPlayingBuilder?) {
+        tvOsOnly = tvos
+    }
+}
