9945d6631bc72fff1136c023c46f77bd50fc2d4e
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-04-19 17:33:34 -0300

    initial commit

---
+++ Example/Example/Example.h
@@ -0,0 +1,19 @@
+//
+//  Example.h
+//  Example
+//
+//  Created by Ricardo Cavalcanti on 19/04/19.
+//  Copyright © 2019 Example org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Example.
+FOUNDATION_EXPORT double ExampleVersionNumber;
+
+//! Project version string for Example.
+FOUNDATION_EXPORT const unsigned char ExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Example/PublicHeader.h>
+
+
---
+++ Example/Example_tvOS/Example_tvOS/Example_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Example_tvOS.h
+//  Example_tvOS
+//
+//  Created by Ricardo Cavalcanti on 19/04/19.
+//  Copyright © 2019 Example org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Example_tvOS.
+FOUNDATION_EXPORT double Example_tvOSVersionNumber;
+
+//! Project version string for Example_tvOS.
+FOUNDATION_EXPORT const unsigned char Example_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Example_tvOS/PublicHeader.h>
+
+
